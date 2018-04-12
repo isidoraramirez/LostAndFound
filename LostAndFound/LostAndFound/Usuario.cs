2b1f3389b7e037b31111465fd71361f1b0a530fc
isidoraramirez <37889372+isidoraramirez@users.noreply.github.com>
2018-04-12 00:37:23 -0300

    Add files via upload

---
+++ LostAndFound/LostAndFound/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostAndFound
+{
+    class Usuario
+    {
+        string rut;
+        char contrasenia;
+        string nombre_usuario;
+        float calificacion;
+        string mail;
+        //List<float> lista_calificaion;
+
+        Usuario(string rut, char contrasenia, string nombre_usuario, float calificacion, string mail)
+        {
+            this.rut = rut;
+            this.contrasenia = contrasenia;
+            this.nombre_usuario = nombre_usuario;
+            this.calificacion = calificacion;
+        }
+    }
+}
